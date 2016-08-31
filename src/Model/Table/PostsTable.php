b47f238815a74ddf5140c55c959972d82c24e078
Ashot <baggi24@mail.ru>
2016-09-01 00:37:59 +0400

    create Tables

--- src/Model/Table/PostsTable.php
+++ src/Model/Table/PostsTable.php
@@ -3,11 +3,41 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Validation\Validator;
 
 class PostsTable extends Table
 {
     public function initialize(array $config)
     {
-        $this->displayField('body');
+//        $this->displayField('body');
+        $this->addBehavior('Timestamp');
+    }
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->requirePresence('title')
+            ->notEmpty('title', __('You need to provide a title'))
+            ->add('title', [
+                'minLength' => [
+                    'rule' => ['minLength', 5],
+                    'message' => 'Titles need to be at least 5 characters long'
+                ],
+                'validFormat' => [
+                    'rule' => 'email',
+                    'message' => 'E-mail must be valid'
+                ]
+
+            ])
+            ->add('title', 'unique', [
+                'rule' => 'validateUnique',
+                'provider' => 'table',
+                'message' => 'The title is not unique enough'
+            ]);
+
+//            ->requirePresence('body')
+//            ->notEmpty('body', __('You need to provide a body'));
+
+
+        return $validator;
     }
 }
